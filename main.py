c987a7beba68706e5a0fa57b7099a5519776fd20
deniswen <w1273287934@gmail.com>
2022-06-17 17:51:45 +0800

    init

--- main.py
+++ main.py
@@ -1,16 +1,40 @@
-# This is a sample Python script.
+import argparse
 
-# Press ⌃R to execute it or replace it with your code.
-# Press Double ⇧ to search everywhere for classes, files, tool windows, actions, and settings.
-
-
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press ⌘F8 to toggle the breakpoint.
+from pyhive import hive
 
 
-# Press the green button in the gutter to run the script.
+def init_argparse():
+    parser = argparse.ArgumentParser()
+
+    # Adding optional argument
+    parser.add_argument("-i", "--hive-host-address", help="hive address", required=True)
+    parser.add_argument("-p", "--port", help="hive port", required=True)
+    parser.add_argument("-u", "--username", help="hive username", required=True)
+    parser.add_argument("-d", "--database", help="database", required=True)
+    parser.add_argument("-f", "--hdfs-address", help="hadoop hdfs address", required=True)
+
+    # Read arguments from command line
+    args = parser.parse_args()
+
+    return args
+
+
+def connect_to_hive():
+    conn = hive.Connection(host="39.103.230.188", port=30115, username="dehoop", database="rd_dev")
+
+
+def get_hive_table(conn):
+    cursor = conn.cursor()
+    cursor.execute("show tables")
+
+    table_name = []
+
+    for table in cursor.fetchall():
+        table_name.append("".join(table))
+
+    return table_name
+
+
 if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+    init_argparse()
+    # connect_to_hive()
